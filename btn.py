62a70b3c37b799149bf8775ca7e15c468e9a0a9f
seabeatrio <soshnin.maks@bk.ru>
2023-06-07 12:09:35 +0300

    ыуф

--- btn.py
+++
@@ -1,6 +0,0 @@
-largest = -1
-for _ in range(3):
-    num = int(input())    
-    if num > largest:
-        largest = num
-print('Наибольшее число равно', largest) 
---
+++ swiper-slider.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesperview: "auto",
+  spacebetween:30 ,
+  grabCurcor:true,
+  navigation: {
+    nextEL: ".swiper-button-next",
+    prevEL: ".swiprer-button-prev ",
+  },
+});
